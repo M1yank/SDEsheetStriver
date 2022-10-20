7d4341a21062400bd8bd9e34f1953dca183a07f0
Mayank Bansal <95163425+M1yank@users.noreply.github.com>
2022-10-20 15:30:43 +0530

    Time: 12 ms (51.64%), Space: 8.3 MB (78.98%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,17 +8,53 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         if(head==nullptr || head->next==nullptr) return head;
+//         ListNode *newhead = nullptr;
+//         while(head != nullptr){
+//             ListNode *next = head->next;
+//             head->next = newhead;
+//             newhead = head;
+//             head = next;
+//         }
+//         return newhead;
+//     }
+// };
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if(head==nullptr || head->next==nullptr) return head;
-        ListNode *newhead = nullptr;
-        while(head != nullptr){
-            ListNode *next = head->next;
-            head->next = newhead;
-            newhead = head;
-            head = next;
+        //iterative
+        if(!head || !head->next) return head;
+        
+        ListNode *prev = nullptr;
+        ListNode *curr = head;
+        ListNode *newh = head->next;
+        
+        while(curr){
+            curr->next = prev;
+            prev = curr;
+            curr = newh;
+            if(newh) newh = newh->next;
         }
-        return newhead;
+       return prev;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
